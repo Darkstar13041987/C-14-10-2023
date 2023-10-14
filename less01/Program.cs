070d89b9306ec4321a4b540bbabcf51dbfdd1d17
mamykin <mamykin_87@mail.ru>
2023-10-14 11:24:15 +0300

    Less01

---
+++ less01/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+// Console.WriteLine("Введите число:");
+//  int number =Convert.ToInt32(Console.ReadLine()); 
+// {
+//     Console.WriteLine(number*number);
+// }
+ 
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int result = int.Parse(number);
+Console.WriteLine(result*result);
