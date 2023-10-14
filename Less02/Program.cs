3cdc601ebe7220f9f7c1f35fb68239dfaf1b0915
mamykin <mamykin_87@mail.ru>
2023-10-14 14:24:35 +0300

    less02

--- Less02/Program.cs
+++ Less02/Program.cs
@@ -4,21 +4,33 @@
 // a = 9, b = -3 -> да 
 // a = -3 b = 9 -> нет
 
-Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine()); 
 
-if 
-(number1 == number2*number2) 
+// if 
+// (number1 == number2*number2) 
+// {
+// Console.WriteLine("Первое Число является квадратом второго");
+// }
+// else
+// {
+// Console.WriteLine("Первое Число не является квадратом второго");
+
+// }
+
+Console.WriteLine("Введите первое число");
+ int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+if
+(num1 == num2*num2)
 {
-Console.WtiteLine("Первое Число является квадратом второго");
+Console.WriteLine("Второе число является квадратом первого");
 }
 else
-{
-Console.WtiteLine("Первое Число не является квадратом второго");
-
-}
+Console.WriteLine("Второе число не является квадратом первого");
 
 
 // Console.Write("Введите первое число: ");
