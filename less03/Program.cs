5dca9d6811acd6ae6ad1814654a180f8e0f7c637
mamykin <mamykin_87@mail.ru>
2023-10-14 15:09:46 +0300

    Less3

--- less03/Program.cs
+++ less03/Program.cs
@@ -6,6 +6,18 @@
 // 	456 -> 6
 // 	782 -> 2
 // 	918 -> 8
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = num;
+// for(i=-num;i<=num;i++)
+// {
+//   Console.Write(i+","); 
+// }
+
+
+
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int i = - number;
