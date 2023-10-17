91e518940f7f20ce24e367b2e11462762b6ee948
mamykin <mamykin_87@mail.ru>
2023-10-17 20:58:22 +0300

    HW_task8

---
+++ HW task 8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу,
+//  которая на вход принимает число (N), 
+//  а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// Console.WriteLine("Введите число:"); //вариант 1
+// int num = Convert.ToInt32(Console.ReadLine());
+// for
+// ( int i = 1; i<num; i++)
+//  if (i % 2 == 0)
+//         Console.WriteLine(i);
+ 
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+//        int res = 1;
+//        while (res < num)
+//        {
+//         Console.WriteLine(res+1);
+//         res+= 2;
+//        }
+ 
